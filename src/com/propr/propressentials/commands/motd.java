413d06251f5c1fc576abb8f185c3157da16d2141
proprdev <propruhh@gmail.com>
2021-07-05 17:25:54 -0500

    remove unused impor

--- src/com/propr/propressentials/commands/motd.java
+++ src/com/propr/propressentials/commands/motd.java
@@ -6,8 +6,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-
-import java.io.File;
 import java.io.IOException;
 
 public class motd implements CommandExecutor {
